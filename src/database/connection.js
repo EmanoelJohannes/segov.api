416908172ddbe0c6826a0d18fad242a46c828336
Emanoel Johannes <emanoeljohannes@hotmail.com>
2021-10-20 05:38:50 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -11,10 +11,11 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host        : '187.45.179.106',
-        user        : 'segov_sicgesp',
-        password    : 'YIZ4AX3WjP^U',
-        database    : 'segov_sicgesp'
+        host         : '187.45.179.106',
+        user         : 'segov_sicgesp',
+        password     : 'YIZ4AX3WjP^U',
+        database     : 'segov_sicgesp',
+        insecureAuth : true
     }
 });
 
